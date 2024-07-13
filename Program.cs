5a26603ef977be5dfb9293ed5c5298627143819e
MARWA11MO <arwakorani20@gmail.com>
2024-07-13 10:13:02 +0300

    C# BASIC

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿namespace lab1_c_
+{   class student
+    {
+        string name;
+        int age;
+        int ssn;
+        string phone;
+
+      //  student(string name, int age, int ssn, string phone)
+      //  {
+      //      this.name = name;
+      //      this.age = age;
+       //     this.ssn = ssn;
+     //       this.phone = phone;
+     //   }
+    } 
+
+  
+    
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //  Console.Write("enter your name: ");
+            //   string name=Console.ReadLine();
+            //    Console.Write("enter your id : ");
+            //   int id = int.Parse(Console.ReadLine());
+            //   Console.Write("enter your age: ");
+            //  int age;
+            //  do
+            //  {
+            //      Console.Write("enter your age: ");
+            //     age = int.Parse(Console.ReadLine());
+
+            //  } while (age < 0);
+
+            //  Console.WriteLine($"data for employee \n name : {name} \n  id: {id}  \n  age : {age}   ");
+
+
+            student ST1 = new student();
+            student ST2 = new student();
+            student ST3 = new student();
+            
+
+
+
+
+        }
+    }
+}
